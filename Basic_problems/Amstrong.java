ec64933b721f35fc0042566818bbb03e1178deec
Pugalenthi <pugal996501@gmail.com>
2025-02-25 23:00:51 +0530

    Update Amstrong.java

--- Basic_problems/Amstrong.java
+++ Basic_problems/Amstrong.java
@@ -1,11 +1,14 @@
 import java.util.*;
+// this code only for three digit numbers
 class Amstrong{
   public static void main (String[] args){
     Scanner sc = new Scanner(System.in);
     int n= sc.nextInt();
+    // org for comparing the last sum and the n value we cannot comare sum with n because n undergoes n/10 so n value will change  
     int org = n;
     int sum = 0;
     while(n>0){
+      // n%10 for getting the last digit for each iteration
       int id = n%10;
       sum = sum+ (int) Math.pow(id,3);
       n= n/10;
