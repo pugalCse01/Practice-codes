adbae113f27acf6d6397b972a08e71a686119365
Pugalenthi <pugal996501@gmail.com>
2025-02-25 22:56:36 +0530

    Update Amstrong.java

--- Basic_problems/Amstrong.java
+++ Basic_problems/Amstrong.java
@@ -1,1 +1,20 @@
-
+class Amstrong{
+  public Static void main (String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n= sc.nextInt();
+    int org = n;
+    int sum = 0;
+    while(n>0){
+      int id = n%10;
+      sum = sum+ (int) Math.pow(id,3);
+      n= n/10;
+    }
+    if(sum == org){
+      System.out.println(" it is a amstrong number");
+    }
+    eslse{
+      System.out.println("it is not a amstrong number");
+    }
+      
+  }
+}
