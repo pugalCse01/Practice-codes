2b311703a0add30337381633b3f62d3c3b1efac5
Pugalenthi <pugal996501@gmail.com>
2025-04-03 23:40:29 +0530

    Create Right_Angled_Triangle_Pattern.java

---
+++ Pattern/Right_Angled_Triangle_Pattern.java
@@ -0,0 +1,10 @@
+class  Right_Angled_Triangle_Pattern{
+    public static void main(String[] args) {
+        int n =5;
+          for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+}
