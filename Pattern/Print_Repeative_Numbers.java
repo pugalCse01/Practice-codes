aa3e9ced7ae6c33cc4e6fd3b789b37f84e9d6d58
Pugalenthi <pugal996501@gmail.com>
2025-04-06 23:14:33 +0530

    Create Print_Repeative_Numbers.java

---
+++ Pattern/Print_Repeative_Numbers.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Print_Repeative_Numbers  {
+    public static void main(String[] args) {
+        int n = 10;
+         for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
