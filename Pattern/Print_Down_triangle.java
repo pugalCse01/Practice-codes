fbd90246e7d361b83c202d315eac54530cbda6e3
Pugalenthi <pugal996501@gmail.com>
2025-04-06 23:21:45 +0530

    Create Print_Down_triangle.java

---
+++ Pattern/Print_Down_triangle.java
@@ -0,0 +1,11 @@
+class Print_Down_triangle {
+    public static void main(String[] args) {
+        int n = 10;
+         for(int i = n;i>0;i--){
+            for(int j = 1;j<i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
