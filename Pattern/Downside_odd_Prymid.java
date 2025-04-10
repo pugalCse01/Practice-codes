2a1702f2906cbc162ae7a54ce3ef8463f53df151
Pugalenthi <pugal996501@gmail.com>
2025-04-10 23:43:52 +0530

    Create Downside_odd_Prymid.java

---
+++ Pattern/Downside_odd_Prymid.java
@@ -0,0 +1,16 @@
+class Downside_odd_Prymid {
+    public static void main(String[] args) {
+       int n= 5;
+        // code here
+        for (int i =0;i<n;i++){
+            
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k<2*(n-i)-1;k++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
